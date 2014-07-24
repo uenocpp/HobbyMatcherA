786138462725ed0fcb74bebf021fa2ec62d36282
ueno <ueno.cpp@gmail.com>
2014-07-24 17:54:25 +0900

    Android beam動作確認

--- src/com/example/hobbymatcher/Beam.java
+++ src/com/example/hobbymatcher/Beam.java
@@ -1,16 +1,22 @@
 package com.example.hobbymatcher;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateNdefMessageCallback;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class Beam extends Activity implements CreateNdefMessageCallback {
 
+	private static final String TAG = "BEAM";
 	NfcAdapter mNfcAdapter;
 
 	@Override
@@ -30,8 +36,35 @@ public class Beam extends Activity implements CreateNdefMessageCallback {
 	
 	@Override
 	public NdefMessage createNdefMessage(NfcEvent event) {
-		// TODO Auto-generated method stub
-		return null;
+		String text = "beam beam beam";
+		NdefMessage msg = new NdefMessage(
+				new NdefRecord[]{ NdefRecord.createMime(
+						"application/com.example.hobbymatcher", text.getBytes())});
+		Log.d(TAG, "creating ndef message: " + text );
+		return msg;
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		if( NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction()) ){
+			processIntent( getIntent() );
+		}
+	}
+	
+	@Override
+	public void onNewIntent( Intent intent ){
+		setIntent( intent );
+	}
+	
+	void processIntent( Intent intent ){
+		TextView textView = (TextView) findViewById(R.id.beam_message);
+		Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+				NfcAdapter.EXTRA_NDEF_MESSAGES);
+		NdefMessage msg = (NdefMessage) rawMsgs[0];
+		String str = new String( msg.getRecords()[0].getPayload() );
+		textView.setText( str );
+		Log.d( TAG, "get message: " + str );
 	}
 
 }
